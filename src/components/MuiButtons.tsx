a46e10adf03979fa6df6863b650fbe35bae0d7c4
Suman <spokhrel196@gmail.com>
2022-03-25 19:57:21 +0530

    Basic button props covered

--- src/components/MuiButtons.tsx
+++ src/components/MuiButtons.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, Stack } from '@mui/material'
+import { Button, Stack,IconButton } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send';
 
 
 function MuiButtons() {
@@ -15,7 +16,7 @@ function MuiButtons() {
                </Stack>
 
                {/* Using Default Color Pallet  */}
-
+               {/* Button Color  */}
                <Stack spacing={2} direction={`row`}>
                     <Button variant="outlined" color='primary'>Primary</Button>
                     <Button variant="outlined" color='secondary'>secondary</Button>
@@ -25,6 +26,7 @@ function MuiButtons() {
                     <Button variant="outlined" color='success'>success</Button>
 
                </Stack>
+               {/* Button Color  */}
                <Stack spacing={2} direction={`row`}>
                     <Button variant="contained" color='primary'>Primary</Button>
                     <Button variant="contained" color='secondary'>secondary</Button>
@@ -34,6 +36,7 @@ function MuiButtons() {
                     <Button variant="contained" color='success'>success</Button>
 
                </Stack>
+               {/* Button Color  */}
                <Stack spacing={2} direction={`row`}>
                     <Button variant="text" color='primary'>Primary</Button>
                     <Button variant="text" color='secondary'>secondary</Button>
@@ -42,6 +45,52 @@ function MuiButtons() {
                     <Button variant="text" color='info'>info</Button>
                     <Button variant="text" color='success'>success</Button>
 
+               </Stack>
+               {/* Button Sizes  */}
+               <Stack  spacing={2} direction={`row`}>
+                    <Stack spacing={2}>
+                    <Button variant='text' size='small' >Small</Button>
+                    <Button variant='text' size='medium' >meduim</Button>
+                    <Button variant='text' size='large' >Large</Button>
+                    </Stack>
+                    
+                    <Stack spacing={2}>
+                    <Button variant='outlined' size='small' >Small</Button>
+                    <Button variant='outlined' size='medium' >meduim</Button>
+                    <Button variant='outlined' size='large' >Large</Button>
+                    </Stack>
+                    
+                    <Stack spacing={2}>
+                    <Button variant='contained' size='small' >Small</Button>
+                    <Button variant='contained' size='medium' >meduim</Button>
+                    <Button variant='contained' size='large' >Large</Button>
+                    </Stack>
+                    
+               
+               </Stack>
+
+               {/* Buttons With Icons  */}
+               <Stack spacing={2} direction='row'>
+                    <Button variant='contained' startIcon={<SendIcon />}>Send</Button>
+                    <Button variant='contained' endIcon={<SendIcon />}>Send</Button>
+
+                    <IconButton aria-label='send'color='success' size='large'>
+                         <SendIcon />
+                    </IconButton>
+
+               </Stack>
+
+               
+               <Stack spacing={2} direction='row'>
+               {/* Buttons without Elevations */}
+               <Button variant='contained' disableElevation startIcon={<SendIcon />}>Send</Button>
+               {/* Button Without Ripple Effect  */}
+               <Button variant='contained' disableRipple endIcon={<SendIcon />}>Send</Button>
+               </Stack>
+
+               {/* OnClick Methods  */}
+               <Stack direction='row'>
+                    <Button variant='contained' color='primary' size='medium' onClick={()=>alert("OnClick Method tested")}>Alert</Button>
                </Stack>
 
                
