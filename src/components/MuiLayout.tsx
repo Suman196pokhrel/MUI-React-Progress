f0c6e4ff7c230983f5bb2137689b68c1879f836b
Suman <spokhrel196@gmail.com>
2022-03-27 07:52:47 +0530

    Paper component completed

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,46 +1,51 @@
 import React from 'react'
-import { Box, Stack, Divider, Grid } from '@mui/material'
+import { Box, Stack, Divider, Grid, Paper } from '@mui/material'
 
 function MuiLayout() {
-  return (<>
-    <Stack sx={{
-      border: '1px solid'
+  return (
+    <Paper sx={{
+      padding: '32px'
     }}
-      direction='row'
-      spacing={2}
-      divider={<Divider orientation='vertical' flexItem />}
+    elevation={4}
     >
-      <Box sx={{
-        backgroundColor: 'primary.main',
-        color: 'white',
-        height: "100px",
-        width: "100px",
-        padding: "16px",
-        "&:hover": {
-          backgroundColor: "primary.light"
-        }
+      <Stack sx={{
+        border: '1px solid'
+      }}
+        direction='row'
+        spacing={2}
+        divider={<Divider orientation='vertical' flexItem />}
+      >
+        <Box sx={{
+          backgroundColor: 'primary.main',
+          color: 'white',
+          height: "100px",
+          width: "100px",
+          padding: "16px",
+          "&:hover": {
+            backgroundColor: "primary.light"
+          }
 
-      }}>
-        Suman pokhrel
-      </Box>
-      <Box
-        display='flex'
-        height='100px'
-        width="100px"
-        bgcolor="success.light"
-        p={2}>
+        }}>
+          Suman pokhrel
+        </Box>
+        <Box
+          display='flex'
+          height='100px'
+          width="100px"
+          bgcolor="success.light"
+          p={2}>
 
-      </Box>
-    </Stack>
-    <Grid container my={4} spacing={2}>
+        </Box>
+      </Stack>
+      <Grid container my={4} spacing={2}>
         <Grid item xs={12} sm={6} md={3} lg xl={3}> <Box bgcolor='primary.light' border='1px solid' p={2}>Item 1</Box></Grid>
         <Grid item xs={12} sm={6} md={3} lg xl={3}> <Box bgcolor='primary.light' border='1px solid' p={2}>Item 2</Box></Grid>
         <Grid item xs={12} sm={6} md={3} lg xl={3}> <Box bgcolor='primary.light' border='1px solid' p={2}>Item 3</Box></Grid>
         <Grid item xs={12} sm={6} md={3} lg xl={3}> <Box bgcolor='primary.light' border='1px solid' p={2}>Item 4</Box></Grid>
 
-    </Grid>
-  </>
-  ) 
+      </Grid>
+    </Paper>
+  )
 }
 
 export default MuiLayout
