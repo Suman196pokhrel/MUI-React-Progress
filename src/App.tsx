860b701091dd37d5b24168c30b1e821cd7412b8e
Suman <spokhrel196@gmail.com>
2022-04-05 16:20:42 +0530

    alert component

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import MuiList from './components/MuiList';
 import MuiChip from './components/MuiChip';
 import MuiToolTip from './components/MuiToolTip';
 import MuiTable from './components/MuiTable';
+import MuiAlert from './components/MuiAlert';
 
 
 function App() {
@@ -52,7 +53,8 @@ function App() {
       {/* <MuiList /> */}
       {/* <MuiChip /> */}
       {/* <MuiToolTip /> */}
-      <MuiTable />
+      {/* <MuiTable /> */}
+      <MuiAlert />
     </div>
   );
 }
